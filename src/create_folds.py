8e99ae3ca8b21760a2250e4425d4adc24e6eeb94
Jan <malinowwskijasiek1999@gmail.com>
2020-10-20 01:38:42 +0200

    Code format

--- src/create_folds.py
+++ src/create_folds.py
@@ -3,14 +3,14 @@
 import pandas as pd
 from sklearn import model_selection
 
-if __name__ == '__main__':
-    train = pd.read_pickle('input/train.pkl')
-    train['kfold'] = -1
+if __name__ == "__main__":
+    train = pd.read_pickle("input/train.pkl")
+    train["kfold"] = -1
 
     kf = model_selection.KFold(n_splits=3, random_state=42)
 
     for (fold, (train_idx, val_idx)) in enumerate(kf.split(X=train)):
-        print (len(train_idx), len(val_idx))
-        train.loc[val_idx, 'kfold'] = fold
+        print(len(train_idx), len(val_idx))
+        train.loc[val_idx, "kfold"] = fold
 
-    train.to_pickle('input/train_folds.pkl')
+    train.to_pickle("input/train_folds.pkl")
